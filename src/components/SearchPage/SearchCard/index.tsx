97b99607866b998e95d46a5c0bf627d0de6ee325
Austin Chappell <austin.w.chappell@gmail.com>
2021-01-08 23:34:17 -0600

    make search cards links to show page

--- src/components/SearchPage/SearchCard/index.tsx
+++ src/components/SearchPage/SearchCard/index.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
+import { paths } from 'constants/paths';
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import UserInfo from './UserInfo';
 
 export const Card = styled.div({
@@ -11,32 +13,35 @@ export const Card = styled.div({
 export const AvatarContainer = styled.div({
   border: '1px solid lightgray',
   borderRadius: 8,
-  boxShadow: '0 3px 6px lightgray, 0 3px 6px',
+  boxShadow: '0 3px 6px lightgray, 0 3px 6px lightgray',
   overflow: 'hidden',
 });
 
 export const Avatar = styled.div({
-  position: 'relative',
   height: '200px',
+  position: 'relative',
   width: '200px',
 });
 
 const SearchCard: FC<Profile> = ({
   age = 99,
   handle = '',
+  id,
   location = '',
   photoCount = 0,
   photoUrl = '',
 }) => (
-  <Card>
-    <AvatarContainer>
-      <Avatar>
-        <img src={photoUrl} alt="potential date" />
+  <Link to={`/${paths.profiles}/${id}`}>
+    <Card>
+      <AvatarContainer>
+        <Avatar>
+          <img src={photoUrl} alt="potential date" />
 
-        <UserInfo age={age} handle={handle} location={location} photoCount={photoCount} />
-      </Avatar>
-    </AvatarContainer>
-  </Card>
+          <UserInfo age={age} handle={handle} location={location} photoCount={photoCount} />
+        </Avatar>
+      </AvatarContainer>
+    </Card>
+  </Link>
 );
 
 export default SearchCard;
