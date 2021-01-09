22d840f1a454358f263b29d11a15cdde2313e3d2
Austin Chappell <austin.w.chappell@gmail.com>
2021-01-09 02:16:14 -0600

    remove unused code

--- src/api/profiles/hooks.ts
+++ src/api/profiles/hooks.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { ProfileContext } from 'state/ProfilesContextProvider';
 import { fetchProfiles } from '.';
 
